ba7ace28790ba89d5b77cb381422b7edacf481b9
ItsATarp <weilu345@gmail.com>
2022-04-23 21:33:10 -0700

    book creation, js not working yet

--- javascript.js
+++ javascript.js
@@ -1,29 +1,69 @@
 let popUpForm = document.querySelector(".popUp");
 let newbutton = document.querySelector(".make");
-console.log(newbutton);
+let closebutton = document.querySelector(".close");
 newbutton.addEventListener('click', () => popUpForm.style.display = 'block');
+closebutton.addEventListener('click', () =>popUpForm.style.display = 'none');
 
-
+let add = document.getElementById('addButton');
+add.addEventListener('click', addToLibrary);
 
 
 let cards = document.querySelector("cards");
 let myLibrary = [];
-function Book(title,author,pages){
+function Book(title,author,pages,read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-}
-
-Book.prototype.info = function(){
-    let s = title + ", " + author + ", " + pages + " pages";
-    read ? s += " read" : s += " not read yet"; 
-    return s;
+    this.read = read;
 }
 
 function addToLibrary(){
+    event.preventDefault();
+    popUpForm.display.style = 'none';
+
+    let b = new Book(title,author,pages,read);
     myLibrary.push(b);
+    setBook();
+    updateDisplay();
+    form.reset();
 }
 
-function makeform(){
-    let newform = document.querySelector
+function updateDisplay(){
+    const display = document.querySelector(".cards");
+    const books = document.querySelectorAll(".book");
+    books.forEach(book => display.removeChild(book));
+
+    for(let i = 0; i < myLibrary.length; i++){
+        createBook(myLibrary[i]);
+    }
+}
+
+function createBook(book){
+    const cards = document.querySelector(".cards");
+    const card = document.createElement('div'); 
+
+    const title = document.createElement('div');
+    title.textContent = book.title;
+
+    const author = document.createElement('div');
+    author.textContent = book.author;
+
+    const pages = document.createElement('div');
+    pages.textContent = book.pages;
+
+    const read = document.createElement('div');
+    if(book.read == 'yes') read.textContent = 'yes';
+    else read.textContent = 'no';
+
+    cards.appendChild(card);
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    card.appendChild(pages);
+}
+
+function setBook(){
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
 }
